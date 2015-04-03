b88cd821262a7a3079c7b9649ef7b9ff4e68c12a
Jim Pelton <jimpelton@u.boisestate.edu>
2015-04-03 16:50:42 -0600

    added testing solution and shared_test project

---
+++ vs/testing/shared_test/src/quad_test.cpp
@@ -0,0 +1,23 @@
+
+
+#include <geometry/quad.h>
+
+#include <gtest/gtest.h>
+
+
+namespace {
+    class Quad_Test : public ::testing::Test {
+    public:
+        Quad_Test() { }
+        virtual ~Quad_Test() { }
+
+        virtual void SetUp() override { }
+        virtual void TearDown() override { }
+    };
+}
+
+
+TEST_F(Quad_Test, TranslateTest)
+{
+    
+}
