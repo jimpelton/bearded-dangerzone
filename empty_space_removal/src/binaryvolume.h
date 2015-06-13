287d493dc6f17de6cb681696befd4f2a02f73477
Jim Pelton <jimpelton@u.boisestate.edu>
2015-06-13 17:47:42 -0600

    adding summedvolumetable

--- empty_space_removal/src/binaryvolume.h
+++ empty_space_removal/src/binaryvolume.h
@@ -6,8 +6,12 @@
 #include <assert.h>
 
 template<typename ValType, typename In>
-struct BinVolBase 
+class BinVolBase 
 {
+public:
+    BinVolBase() { }
+    virtual ~BinVolBase() { }
+
     ///////////////////////////////////////////////////////////////////////////////
     /// \brief Create a 1's and 0's array from \p in_data. 
     /// \param isEmpty Functor that takes a \c ValType and decides if that voxel is empty.
@@ -15,7 +19,8 @@ struct BinVolBase
     /// \param[out] out_binary Populated with a 1 for non-empty voxels, else 0.
     /// \note \p out_binary will be resized to the length of \p in_data.
     ///////////////////////////////////////////////////////////////////////////////
-    void createBinaryVolume(In in, In end, std::function<bool(ValType)> empty);
+    virtual void 
+    createBinaryVolume(In in, In end, std::function<bool(ValType)> empty);
 
     std::vector<bool> volume;
 };
@@ -38,13 +43,33 @@ void BinVolBase<ValType, In>::createBinaryVolume
     }
 }
 
-template<typename ValType>
-struct BinVol : public BinVolBase<ValType, typename std::vector<ValType>::const_iterator>
-{ };
+//template<typename ValType>
+//class BinVol : public BinVolBase<ValType, typename std::vector<ValType>::const_iterator>
+//{ };
 
 template<typename ValType>
-struct BinVolFromArray : public BinVolBase < ValType, ValType* > 
+class BinVol : public BinVolBase < ValType, ValType* > 
 { };
+
+
+template<typename ValType>
+class SummedVolumeTableBase : public BinVol<ValType>
+{
+public:
+    SummedVolumeTableBase() : BinVolBase() { }
+    virtual ~SummedVolumeTableBase() { }
+
+    virtual void create(ValType *data);
+
+    BinVol<ValType> volume;
+
+};
+
+template<typename ValType>
+void SummedVolumeTableBase<ValType>::create(ValType *data)
+{
+        
+}
 
 #endif // !binaryvolume_h__
 
