4b4408b00eca317196859aae13b28cac6ab15920
Jim Pelton <jimpelton@u.boisestate.edu>
2016-05-01 16:48:37 -0600

    removed preprocessor folder

---
+++ python/makevol/makevol.py
@@ -0,0 +1,19 @@
+__author__ = 'Jim'
+
+import sys
+import math
+
+volSizeBytes = math.pow(2,30)
+
+blockSizeBytes = math.pow(2,10)
+
+
+
+def main(argv):
+
+
+    return
+
+
+if __name__ == '__main__':
+    main(sys.argv)
