2ec6f99c640b6f8828a9a8a7f5acb10381a08f79
Jim Pelton <jpghub@gmail.com>
2019-03-14 08:20:19 -0600

    scripts for runpreproc

--- scripts/runpreproc_hop.sh
+++ scripts/runpreproc_hop.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
 
+d=$1
+outDir=$2
+tfd=$3
+
 for i in {1,16,24,32,48,64,72,96}; do
-    NUMBA_WARNINGS=1 python preproc/src/pyproc.py \
-        --raw /run/media/jim/data/volumes/INL/Josh_Kane/hop_flower/Hop_Flower-Resampled-3509x3787x4096.raw \
-        --out ../sample_volumes/hop_4k_${i}x${i}x${i}.json \
+    NUMBA_WARNINGS=1 python ../preproc/src/pyproc.py \
+        --raw ${d}/Hop_Flower-Resampled-3509x3787x4096.raw \
+        --out ${outDir}/hop-4k-${i}x${i}x${i}-zero_to_one.json \
         --bx ${i} --by ${i} --bz ${i} \
         --vx 3509 --vy 3787 --vz 4096 \
-        --tf /home/jim/james.s.pelton@gmail.com/school/grad_school/JimPelton_Project/transfer_functions/otf/hop_default.otf \
-        --dtype u1
+        --tf ${tfd}/zero_to_one.otf \
+        --dtype u1 | tee ${outDir}/hop-4k-${i}x${i}x${i}-zero_to_one.txt
+done
+
+for i in {1,16,24,32,48,64,72,96}; do
+    NUMBA_WARNINGS=1 python ../preproc/src/pyproc.py \
+        --raw ${d}/Hop_Flower-Resampled-3509x3787x4096.raw \
+        --out ${outDir}/hop-4k-${i}x${i}x${i}-hop_default.json \
+        --bx ${i} --by ${i} --bz ${i} \
+        --vx 3509 --vy 3787 --vz 4096 \
+        --tf ${tfd}/hop_default.otf \
+        --dtype u1 | tee ${outDir}/hop-4k-${i}x${i}x${i}-hop_default.txt
 done
 
 
