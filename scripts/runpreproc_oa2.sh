dc216b073696af2c42ae0e5356fbd2ce35e5376b
Jim Pelton <jpghub@gmail.com>
2019-03-15 07:56:45 -0600

    runpreproc_oa2.sh script

--- scripts/runpreproc_oa2.sh
+++ scripts/runpreproc_oa2.sh
@@ -6,22 +6,22 @@ tfd=$3
 
 for i in {1,16,24,32,48,64,72,96}; do
     NUMBA_WARNINGS=1 python ../preproc/src/pyproc.py \
-        --raw ${d}/ \
-        --out ${outDir}/hop_4k_${i}x${i}x${i}.json \
+        --raw ${d}/oa2-2391x3084x2452-uchar.raw \
+        --out ${outDir}/oa2-4k-${i}x${i}x${i}-zero_to_one.json \
         --bx ${i} --by ${i} --bz ${i} \
-        --vx 3509 --vy 3787 --vz 4096 \
+        --vx 2391 --vy 3084 --vz 2452 \
         --tf ${tfd}/zero_to_one.otf \
-        --dtype u1 | tee ${outDir}/hop-4k-${i}x${i}x${i}-zero_to_one.txt
+        --dtype u1 | tee ${outDir}/oa2-4k-${i}x${i}x${i}-zero_to_one.txt
 done
 
 for i in {1,16,24,32,48,64,72,96}; do
     NUMBA_WARNINGS=1 python ../preproc/src/pyproc.py \
-        --raw ${d}/Hop_Flower-Resampled-3509x3787x4096.raw \
-        --out ${outDir}/hop_4k_${i}x${i}x${i}.json \
+        --raw ${d}/oa2-2391x3084x2452-uchar.raw \
+        --out ${outDir}/oa2-4k-${i}x${i}x${i}-oa2.json \
         --bx ${i} --by ${i} --bz ${i} \
-        --vx 3509 --vy 3787 --vz 4096 \
-        --tf ${tfd}/hop_default.otf \
-        --dtype u1 | tee ${outDir}/hop-4k-${i}x${i}x${i}-hop_default.txt
+        --vx 2391 --vy 3084 --vz 2452 \
+        --tf ${tfd}/oa2-478x616x490-uchar.otf \
+        --dtype u1 | tee ${outDir}/oa2-4k-${i}x${i}x${i}-oa2.txt
 done
 
 
